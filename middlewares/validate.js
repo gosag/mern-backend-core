89de40cf3399e8189bb7c22bf81e023e7e86aa2b
Gosa Girma <gosagirma441@gmail.com>
2026-02-18 10:43:09 +0300

    add validator

---
+++ middlewares/validate.js
@@ -0,0 +1,13 @@
+export default validate=(schema)=>(req,res,next)=>{
+    const result=schema.safeParse(req.body)
+    if(!result.success){
+        return res.status(400).json({
+            errors:result.error.errors.map(err=>({
+                field:err.path[0],
+                message:err.message
+            }))
+        })
+    }
+    req.body=result.data;
+    next
+}
---
+++ models/userSchema.js
@@ -0,0 +1,16 @@
+import {z} from "zod";
+const  userSchema=z.object({
+    userName:z
+    .string()
+    .trim()
+    .min(3,"username should contain at least 3 letters"),
+    password:z
+    .string()
+    .min(6,"password too short"),
+    email:z
+    .string()
+    .trim()
+    .toLowerCase()
+    .email()
+})
+export default userSchema;
