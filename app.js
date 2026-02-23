f122849f2d21bdbbc6477c938b56704ab5205519
Gosa Girma <gosagirma441@gmail.com>
2026-02-23 13:28:51 +0300

    install compression dependencies

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import cors from "cors"
+import compression from "compression"
 import router from './routes/posts.js';
 import userRouter from "./routes/user.js"
 import logger from './middlewares/logger.js';
@@ -7,6 +9,14 @@ const app=express();
 // to parse the incoming request body as JSON
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
+//to allow only some domains access the api
+const corsOptions={
+    origin:"http://localhost:3000",
+    methods:["GET","POST","PUT","DELETE"],
+    alowedHeaders:["Content-Types","Authorization"],
+    cridentials:true
+}
+app.use(cors(corsOptions))
 //using the logger middleware
 app.use(logger);
 app.use("/api/posts",router);
--- server.js
+++ server.js
@@ -1,17 +1,9 @@
 import app from "./app.js"
 import dotenv from "dotenv"
-import cors from "cors"
 import connectDB from "./config/db.js"
 dotenv.config()
 const PORT=process.env.PORT ||8000;
-const corsOptions={
-    origin:"http://localhost:3000",
-    methods:["GET","POST","PUT","DELETE"],
-    alowedHeaders:["Content-Types","Authorization"],
-    cridentials:true
-}
 connectDB()
-app.use(cors(corsOptions))
 //starting the server
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
