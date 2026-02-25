e438da593f71ceb976e79c85e4ab22090ded86f6
Gosa Girma <gosagirma441@gmail.com>
2026-02-25 10:50:42 +0300

    aad typescripte to the postController

--- Controllers/postControllers.ts
+++ Controllers/postControllers.ts
@@ -1,9 +1,20 @@
 import Post from '../models/Post.js';
+import type { Request, Response, NextFunction } from "express";
 import mongoose from "mongoose"
-export const getPosts=async (req,res,next)=>{
+interface GetPostsQuery{
+    limit?:string,
+    page?:string
+}
+ interface RequestError extends Error{
+            status?:number
+         }
+export const getPosts=async ( 
+  req: Request<{}, {}, {}, GetPostsQuery>,
+  res: Response,
+  next: NextFunction)=>{
     try{
-    const limit= Math.min(parseInt(req.query.limit)||10,100)
-    const page=Math.max(parseInt(req.query.page)|| 1,1)
+    const limit= Math.min(parseInt(req.query.limit ?? "10"),100)
+    const page=Math.max(parseInt(req.query.page?? "1"),1)
     const  skip=(page-1)*limit
     const totalItems=await Post.countDocuments()
     const totalPage=Math.ceil(totalItems/limit)
@@ -20,12 +31,18 @@ export const getPosts=async (req,res,next)=>{
     }
     
 }
-export const getByAuthor=async(req,res,next)=>{
-    try{
-        const query={}
+interface getByAuthorQuery{
+    user?:string
+}
+export const getByAuthor=async(req:Request<{},{},{},getByAuthorQuery>,
+    res:Response,
+    next:NextFunction
+)=>{
+    try{    
+        const query:getByAuthorQuery={}
         if(req.query.user){
             if(!mongoose.Types.ObjectId.isValid(req.query.user)){
-                const error= new Error("Invalid author Id")
+                const error= new Error("Invalid author Id") as RequestError
                 error.status=400;
                 return next(error)
             }
@@ -39,22 +56,30 @@ export const getByAuthor=async(req,res,next)=>{
     }
     
 }
-export const getPostsById=async (req,res,next)=>{
+interface paramsInterface{
+    id?:string
+}
+export const getPostsById=async (
+    req:Request<paramsInterface,{},{},{}>,
+    res:Response,
+    next:NextFunction
+)=>{
    const postId=req.params.id;
    const post=await Post.findById(postId);
    if(!post){
-       const error=new Error("Post not found");
+       const error=new Error("Post not found") as RequestError;
        error.status=404;
        return next(error)
    }
    res.status(200).json(post)
 }
-export const getPostsByUserId=async(req,res,next)=>{
+
+export const getPostsByUserId=async(req:Request<paramsInterface,{},{},{}>,res:Response,next:NextFunction)=>{
     try{
          const userId=req.params.id;
-         const postsByUserId=await Post.find({user:userId}).populate("user","userName email");
+         const postsByUserId=await Post.find({user:userId as any}).populate("user","userName email");
          if(!postsByUserId){
-            const error=new Error("Posts under this user is is not found");
+            const error=new Error("Posts under this user is is not found") as RequestError;
             error.status=404;
             return next(error)
          }
@@ -65,7 +90,7 @@ export const getPostsByUserId=async(req,res,next)=>{
     }
    
 }
-export const createPost=async (req,res,next)=>{
+export const createPost=async (req:Request,res:Response,next:NextFunction)=>{
     try{
         const newPost=new Post({
             title:req.body.title,
@@ -78,21 +103,21 @@ export const createPost=async (req,res,next)=>{
         next(error);
     }
 }
-export const updatePost=async (req,res,next)=>{
+export const updatePost=async (req:Request,res:Response,next:NextFunction)=>{
     const postId=req.params.id;
     const post=await Post.findByIdAndUpdate(postId,{title:req.body.title},{ returnDocument: 'after' } )
     if(!post){
-        const error=new Error("Post not found");
+        const error=new Error("Post not found") as RequestError;
         error.status=404;
         return next(error);
     }
     res.json(post);
 }
-export const deletePost=async (req,res,next)=>{
+export const deletePost=async (req:Request,res:Response,next:NextFunction)=>{
     const postId=req.params.id;
     const post=await Post.findByIdAndDelete(postId)
     if(!post){
-        const error=new Error("Post not found");
+        const error=new Error("Post not found") as RequestError;
         error.status=404;
         return next(error);
     }
